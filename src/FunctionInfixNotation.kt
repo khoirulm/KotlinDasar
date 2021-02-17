5ef05c9a06723726da291deb9f0b1d3cae98af36
khoirul muttaqin <khoirulmuttaqin08@gmail.com>
2021-02-17 19:16:34 +0700

    Learn

--- src/FunctionInfixNotation.kt
+++ src/FunctionInfixNotation.kt
@@ -1,12 +1,15 @@
-infix fun String.to(type : String): String {
-    if (type  == "UP"){
+infix fun String.to(type : String) : String{
+    if (type == "UP"){
         return this.toUpperCase()
     }else{
         return this.toLowerCase()
     }
 }
 
-fun main() {
-    val  result = "Khoirul".toUpperCase()
-    println(result)
+fun main(){
+    val result = "Khoirul" to "UP"
+    print(result)
+
+
+    100 downTo 1
 }
