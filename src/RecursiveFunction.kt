87d3e12b5a1f97c776eb560a3a97d8714c6ecff2
khoirul muttaqin <khoirulmuttaqin08@gmail.com>
2021-02-17 19:17:36 +0700

    Learn

--- src/RecursiveFunction.kt
+++ src/RecursiveFunction.kt
@@ -1,22 +1,21 @@
-fun factorial(value:Int):Int{
-    var result = 1
-    for(i in value downTo 1){
-        result *= i
-    }
-
-    return  result
-}
-
-fun factorial2(value: Int):Int{
-    return when(value){
-        1 -> 1
-        else -> value * factorial2(value -1)
-
-    }
-
-}
-
 fun main(){
-    println(factorial(10))
-    println(factorial2(10))
+
+    fun factorialLoop(value : Int) : Int{
+        var result  =  1
+        for(i in value downTo 1 ){
+            result *= i
+        }
+        return result
+    }
+
+    fun factorialRecursive(value : Int):Int{
+        return when (value){
+            1 -> 1
+            else -> value * factorialRecursive(value -1)
+        }
+    }
+
+
+    println(factorialRecursive(10))
+
 }
