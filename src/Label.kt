5fe921a98ced73fcad227fc04ae07c3eeabe469c
khoirul muttaqin <khoirulmuttaqin08@gmail.com>
2021-03-03 17:17:55 +0700

    add file

---
+++ src/Label.kt
@@ -0,0 +1,34 @@
+fun labelBreak(){
+    loopI@for (i in 1..10){
+        loopJ@for (j in 1..10){
+            if(i > 5){
+                break@loopI
+            }
+            println("$i * $j = ${i*j}")
+        }
+    }
+}
+fun labelContinue(){
+    loopI@for (i in 1..10){
+        loopJ@for (j in 1..10){
+            if(i > 5){
+                continue@loopI
+            }
+            println("$i * $j = ${i*j}")
+        }
+    }
+}
+
+
+fun main(){
+
+    fun test(name:String, operation:(String) -> Unit):Unit = operation(name)
+    test("khoirul"){
+        if (it == ""){
+            return@test
+        }else{
+            println{"Khoirul"}
+        }
+    }
+
+}
