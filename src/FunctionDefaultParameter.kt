8fa112371d235f7536caf18a097c1aaf8d356658
khoirul muttaqin <khoirulmuttaqin08@gmail.com>
2021-03-09 23:07:12 +0700

    Belajar

--- src/FunctionDefaultParameter.kt
+++ src/FunctionDefaultParameter.kt
@@ -1,9 +1,8 @@
-fun hello(firstName: String = "Mr", lastName: String = "Ganteng Banget"){
-    println("$firstName $lastName")
+fun hello(firstName:String, lastName:String = ""){
+        println("Hello $firstName $lastName")
 }
 
 fun main() {
     hello("Khoirul")
-    hello()
     hello("Khoirul", "Muttaqin")
 }
